d6ea4ddc3662866f23b367863f2ae6dbf6e8a5f1
AndrewKev <akmkevin100@gmail.com>
2023-01-19 10:46:38 +0700

    footer

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+    },
+    extend: {
+      backgroundImage: {
+        'main': "url('https://ui-assets-gc.thrillophilia.com/assets/homepage/homepage-banner-2696fc25d8bb0f563e9ff7ae22882ee67cea624e244dfb0bc74316db0ffdcfba.jpg')"
+      },
+      colors: {
+        'soft-black': '#2E343D'
+      }
+    },
+  },
+  plugins: [],
+}
